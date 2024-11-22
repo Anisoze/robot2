44fa148b4323d59b6460ea9849eb7484b1e7a0ea
Robohub4 <robohub4@example.com>
2024-11-22 17:33:44 +0300

    AA

---
+++ build/study_pkg_2/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/user_robohub3/catkin_ws/src/study_pkg_2
+# Build directory: /home/user_robohub3/catkin_ws/build/study_pkg_2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ devel/share/my_2_robot_moveit_config/cmake/my_2_robot_moveit_configConfig.cmake
@@ -0,0 +1,1 @@
+/home/user_robohub3/catkin_ws/devel/.private/my_2_robot_moveit_config/share/my_2_robot_moveit_config/cmake/my_2_robot_moveit_configConfig.cmake
